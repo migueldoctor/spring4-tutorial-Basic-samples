707674828e0d09a3497726337fae2690794d595d
mdoctor <mdoctor@192.168.95.199>
2016-09-20 12:21:33 +0200

    Sample 1 of Spring 4 course

---
+++ spring-samples/naturalprogrammer-spring-sample1/src/main/java/com/tutorial/naturalprogrammer/spring/sample/NaturalprogrammerSpringSample1Application.java
@@ -0,0 +1,12 @@
+package com.tutorial.naturalprogrammer.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalprogrammerSpringSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalprogrammerSpringSample1Application.class, args);
+	}
+}
